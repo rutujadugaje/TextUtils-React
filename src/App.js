2b111c348ae9b7bf5cbbf3ca021ce2e3b8538f15
rutujadugaje <dugajerutuja@gmail.com>
2024-09-16 18:48:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
@@ -12,8 +11,8 @@ import {
 } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
-const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light'); 
+  const [alert, setAlert] = useState(null);
 
 const showAlert = (message, type)=>{
   setAlert({
@@ -29,21 +28,12 @@ const showAlert = (message, type)=>{
     if(mode === 'light'){
        setMode('dark');
        document.body.style.backgroundColor = '#042743';
-       showAlert("Dark mode has been enabled", "success");
-       document.title = 'TextUtils - Dark mode';
-      //  setInterval(() => {
-      //   document.title = 'TextUtils is amazing mode';
-      //  }, 2000);
-      //  setInterval(() => {
-      //   document.title ='Install TextUtils now ';
-      //  }, 1500);
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = 'TextUtils - light mode';
-    }
+      }
    
   }
   return (
@@ -51,12 +41,11 @@ const showAlert = (message, type)=>{
       <HashRouter>
           <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
           <Alert alert={alert}/> 
-          {/* <About></About>      */}
           <div className="container my-3">
                 <Routes>
-                      <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to Analyze below"  mode={mode}/>}>                
+                      <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra Spaces"  mode={mode}/>}>                
                       </Route>
-                      <Route path="/about" element={<About about="about"></About>}>
+                      <Route path="/about" element={<About about="about" mode={mode}></About>}>
                       </Route>
                 </Routes>
             </div>
