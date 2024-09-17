687ec49154be9d9a85f369f60e9bc68b4b9b630b
rutujadugaje <dugajerutuja@gmail.com>
2024-09-16 18:45:23 -0700

    Update TextForm.js

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
   const handleUpClick = () =>{
-    // console.log("UpperCase was Clicked" + text);   //for displaing msg on console
+
     let newText = text.toUpperCase();
     setText(newText);
     props.showAlert("Converted to uppercase", "success");
@@ -21,14 +21,14 @@ export default function TextForm(props) {
   }
 
   const handleOnChange = (event) =>{
-    // console.log("On change");   
     setText(event.target.value);
   }
 
   const handleCopy = () => {
-    var text = document.getElementById("myBox");    //from  comment
+    var text = document.getElementById("myBox");  
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Copied to Clipboard!", "success");
   }
 
@@ -37,31 +37,29 @@ export default function TextForm(props) {
     setText(newText.join(" "));
     props.showAlert("Extra spaces removed!", "success");
   }
-  const [text, setText] = useState(' ');  //useState hooks
+  const [text, setText] = useState(' ');  
   
   return (
     <>
     <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-        <h1>{props.heading}</h1>
+        <h1 className='mb-3'>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'gray':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8" onChange={handleOnChange}  ></textarea>
+          <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8" onChange={handleOnChange}  ></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
 
     <div className="container my-3"style={{color: props.mode==='dark'?'white':'#042743'}}  >
       <h2>Your Text Summery</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008 * text.split(" ").length}Minutes Read</p>
+      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}Minutes Read</p>
       <h2 >Preview</h2>
-      <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+      <p>{text.length>0?text:"Nothing to preview!"}</p>
     </div>
     </>
   )
 }
-
-// You can Change TEXT to uppercase from lower without reloading page
